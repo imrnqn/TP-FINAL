f2f0b7cd545d05fbf605b0893779ea642f0a0c0c
cristianchuy <crchuychuy@gmail.com>
2023-10-03 20:28:46 -0300

    @imrnqn

---
+++ src/pfnutricionista/entidades/Horario.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pfnutricionista.entidades;
+
+/**
+ *
+ * @author cdc_3
+ */
+public enum Horario {
+    DESAYUNO,
+    ALMUERZO,
+    MERIENDA,
+    CENA,
+    SNACK
+}
